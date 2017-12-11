856e66a1cd4364701271f02c6f442f544396a123
Alex Felipe <alexfelipevieira@gmail.com>
2017-12-11 11:28:39 -0200

    video 1.2

---
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,15 @@
+package br.com.alura.aluraviagens.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.alura.aluraviagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
