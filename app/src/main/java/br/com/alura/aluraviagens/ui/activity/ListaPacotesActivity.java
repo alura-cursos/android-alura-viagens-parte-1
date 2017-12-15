63194853975f67cf7316770dbcb1fd66400202cf
Alex Felipe <alexfelipevieira@gmail.com>
2017-12-15 09:17:40 -0200

    video 6.1

--- app/src/main/java/br/com/alura/aluraviagens/ui/activity/ListaPacotesActivity.java
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/ListaPacotesActivity.java
@@ -17,6 +17,9 @@ public class ListaPacotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pacotes);
+
+        setTitle("Pacotes");
+
         ListView listaDePacotes = findViewById(R.id.lista_pacotes_listview);
 
         List<Pacote> pacotes = new PacoteDAO().lista();
--- app/src/main/java/br/com/alura/aluraviagens/ui/adapter/ListaPacotesAdapter.java
+++ app/src/main/java/br/com/alura/aluraviagens/ui/adapter/ListaPacotesAdapter.java
@@ -10,7 +10,11 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.List;
+import java.util.Locale;
 
 import br.com.alura.aluraviagens.R;
 import br.com.alura.aluraviagens.model.Pacote;
@@ -58,10 +62,23 @@ public class ListaPacotesAdapter extends BaseAdapter {
         imagem.setImageDrawable(drawableImagemPacote);
 
         TextView dias = viewCriada.findViewById(R.id.item_pacote_dias);
-        dias.setText(pacote.getDias() + "dias");
+        String diasEmTexto = "";
+        int quantidadeDeDias = pacote.getDias();
+        if(quantidadeDeDias > 1){
+            diasEmTexto = quantidadeDeDias + " dias";
+        } else {
+            diasEmTexto = quantidadeDeDias + " dia";
+        }
+        dias.setText(diasEmTexto);
 
         TextView preco = viewCriada.findViewById(R.id.item_pacote_preco);
-        preco.setText(pacote.getPreco().toString());
+        BigDecimal precoDoPacote = pacote.getPreco();
+        NumberFormat formatoBrasileiro = DecimalFormat.getCurrencyInstance(
+                new Locale("pt", "br"));
+        String moedaBrasileira = formatoBrasileiro
+                .format(precoDoPacote)
+                .replace("R$", "R$ ");
+        preco.setText(moedaBrasileira);
 
         return viewCriada;
     }
